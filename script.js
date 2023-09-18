43f4fd9de5c27a759c8a2f7ff78154b856068902
NUR ALAM MONDAL <145200844+NUSH-9922@users.noreply.github.com>
2023-09-18 16:42:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+function btnFun() {
+            document.getElementById("form").style.opacity = 1;
+            document.getElementById("form").style.margin = "-562px auto";
+            document.getElementById("btn").style.transform = "scale(.0)";
+        }
+        function Event1() {
+            let Keydown = document.getElementById("Keydown").value;
+            document.getElementById("form").style.background = Keydown;
+        }
+        function Event2() {
+            let Keypress = document.getElementById("Keypress").value;
+            document.getElementById("form").style.background = Keypress;
+        }
+        function Event3() {
+            let Keyup = document.getElementById("Keyup").value;
+            document.getElementById("form").style.background = Keyup;
+        }
 
